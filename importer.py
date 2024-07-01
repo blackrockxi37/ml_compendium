bee71346833e59f02f60a8e2481213e7f1e14836
blackrockxi37 <agentsmith226@gmail.com>
2024-07-01 18:06:39 +0300

    new importer class

---
+++ importer.py
@@ -0,0 +1,20 @@
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
+#end
+
+class importer():
+    def  __init__(self, split_tabs = 4):
+        print(__file__)
+        libs = list()
+        with open(__file__) as f:
+            libs = f.readlines()
+        index = libs.index('#end\n')
+        libs = libs[:index]
+        libs = [i.replace('\n', '') for i in libs]
+
+        for i in libs:
+            exec(i)
+            split = '\t' * split_tabs
+            print(f'{i}{split}was executed.')
+
+
